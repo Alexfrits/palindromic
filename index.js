08b79cb239265d785cb85ce0ed4087cac0ba0a02
Alexandre Frits <frits.alex@gmail.com>
2016-12-21 22:32:11 +0100

    first working implementation

--- index.js
+++ index.js
@@ -18,7 +18,38 @@ function isPalindrom (str) {
   return reversedRegEx.test(str);
 }
 
-function generateNumbers (initialNum = 0, max = 10, accumulator) {
-  if(initialNum >= max) {return accumulator; }
-  return generateNumbers(initialNum + 1, max, accumulator);
+// doesn't work with large numbers (e.g. more than 7000)
+// function generateNumbers (current = 0, max = 7000, accumulator = []) {
+//   const newAccumulator = [...accumulator, current];
+//   if(current >= max) {return accumulator; }
+//   return generateNumbers(current + 1, max, newAccumulator);
+// }
+
+// starting at 1 because we don't account leading 0
+// this works but is pretty slow with 10e6
+function generateNumbers (current = 1, max = 10) {
+  const accumulator = [];
+  while (current < max) {
+    current++;
+    if(isPalindrom(toDecStr(current)) && isPalindrom(toBinStr(current))) {
+      accumulator.push(current);
+    }
+  }
+
+  return accumulator;
 }
+
+// test for a second implementation
+// TODO: separate the main iteration to multiple, smaller ones
+
+// const from1to10e5 = generateNumbers(1, 10e5);
+// const from10e5to20e5 = generateNumbers(10e5, 20e5);
+// const from20e5to30e5 = generateNumbers(20e5, 30e5);
+
+// function pumpIt () {
+//   for(let i = 0; i < 10, i++) {
+
+//   }
+// }
+
+// idea for a third implementation: try with a decreasing for loop
